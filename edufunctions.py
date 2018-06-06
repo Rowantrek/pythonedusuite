dea99077529a6dee858f7c1452bf67c92cc94097
Rowantrek <Rowan@nrgnetwork.org>
2018-06-06 17:31:02 +1000

    Major Update - 007
    Added Register and Settings Menu
    Register Menu fully functional
    Settings Menu WIP
    DB For Regitsre is complete

--- edufunctions.py
+++ edufunctions.py
@@ -12,7 +12,7 @@ class DB():
 		global cursor
 		connection = pymysql.connect(host='localhost',user='root',password='root',db='sdd',charset='utf8mb4')
 		cursor = connection.cursor()
-
+		
 class LoginCheck():
 	def init(username,password):
 		DB()
@@ -23,8 +23,33 @@ class LoginCheck():
 			userid = row[0]
 			user = row[1]
 			passw = row[2]
+			email = row[3]
+			dob = row[4]
 		if (username.lower() == user.lower() and password == passw):
 			settings.name = user
+			settings.email = email
+			settings.dob = dob
 			return True
 		else:
 			return False
+		connection.close()
+		
+class RegisterCheck():
+	def init(username,password,email,dob):
+		#CALLING DB CONNECT
+		DB()
+		insertRequest = "INSERT INTO `users` (`username`, `password`, `email`, `dob`) VALUES (%s, %s, %s, %s)"
+		cursor.execute(insertRequest, (username, password, email, dob))
+		connection.commit()
+		sql = "SELECT * FROM USERS WHERE `USERNAME`=%s"
+		cursor.execute(sql, (username))
+		results = cursor.fetchall()
+		for row in results:
+			userid = row[0]
+			user = row[1]
+			passw = row[2]
+		if (username.lower() == user.lower()):
+			return True
+		else:
+			return False
+		connection.close()
--- settings.py
+++ settings.py
@@ -7,3 +7,7 @@
 def init():
 	global name
 	name = "Guest"
+	global email
+	email = "Email"
+	global dob
+	dob = "00-00-0000"
