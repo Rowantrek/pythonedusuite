e175beb29349ed6c3ed8094bd3ac7df81da85dcc
Rowantrek <Rowan@nrgnetwork.org>
2018-07-02 22:46:56 +1000

    ADDED MAJOR IMAGES

--- edugui.py
+++ edugui.py
@@ -10,7 +10,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic, QtWebKitWidgets
 #IMPORT EduSuite Functions
 import edufunctions
 #IMPORT MAIN GUI
-import main_rc
+import resources
 #IMPORT SETTINGS
 import settings
 
